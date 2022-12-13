bb071b74c3b4494dd188f6af1a1aba67fe261ac1
Chirag Reddy Jitta <chiragreddy406@gmail.com>
2022-12-12 22:08:51 -0700

    Program to Interfaces

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Design-Principles-and-Design-Algorithms
+
+DESIGN PRINCIPLES
+
+Principles-1
+
+1. Encapsulate what varies
+2. Composition Over Inheritance
+3. Loose Coupling
+4. Program to Interfaces
---
+++ patterns-and-principles/src/main/java/Principles1/ProgramToInterfaces/MainClass.java
@@ -0,0 +1,9 @@
+package Principles1.ProgramToInterfaces;
+
+public class MainClass{
+
+	// Program to interfaces -> program to super types (Polymorphism) i.e. classes should use abstract classes/interfaces instead of specific class implementation directly
+	// Use interfaces or abstract classes when possible rather than concrete classes - Similar to Loose Coupling design principle but also applies for abstract classes
+	
+	
+}
